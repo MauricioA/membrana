f6508be940e76b9668d4d2a5e0f797b3a3fd02a4
Mauricio_repo <Mauricio_repo@4286a94c-6050-4a66-afa6-785fb32b8390>
2014-01-14 22:04:52 +0000

    a

--- membrana/FEM_termico/Mesh2d/celula.m
+++ membrana/FEM_termico/Mesh2d/celula.m
@@ -1,4 +1,4 @@
-n = 64;
+n = 32;
 h = 100;
 r = 10;
 d = 0.1
@@ -7,7 +7,7 @@ dtheta = pi/n;
 theta  = (-pi/2 : dtheta : pi/2)';
 memb_i = [cos(theta)*r     sin(theta)*r+h/2];
 memb_o = [cos(theta)*(r+d) sin(theta)*(r+d)+h/2];
-nodes = [0 0; 50 0; 50 100; 0 100];
+nodes  = [0 0; 50 0; 50 100; 0 100];
 
 nodes = vertcat(nodes, memb_i, memb_o);
 
@@ -42,4 +42,55 @@ faces{1} = [1, 2, 3, 4, 8, 8 + size(edges_i)(1) + 1: 8 + size(edges_i)(1) + size
 faces{2} = [5, 7, 9 : 8 + size(edges_i)(1) + size(edges_o)(1)];
 faces{3} = [6, 8 + 1 : 8 + size(edges_i)(1)];
 
-[p, t, fnum] = meshfaces(nodes, edges, faces);
+[p, t, g] = meshfaces(nodes, edges, faces);
+
+f = fopen('nueva.fem', 'w');
+
+nNodos = size(p)(1);
+nElems = size(t)(1);
+
+fprintf(f, '*COORDINATES\n');
+fprintf(f, ' %d\n', nNodos);
+
+for i = 1 : nNodos
+	fprintf(f, '%d %f %f\n', i, p(i, 1), p(i, 2));
+end
+
+fprintf(f, '*ELEMENT_GROUPS\n');
+fprintf(f, ' 3\n');
+
+elemsF = [];
+elemsM = [];
+elemsC = [];
+
+for i = 1 : nElems
+	if   g(i) == 1
+		elemsF(end+1) = i;
+	elseif g(i) == 2
+		elemsM(end+1) = i;
+	elseif g(i) == 3
+		elemsC(end+1) = i;
+	end
+end
+
+fprintf(f, '1 %d Tri3\n', size(elemsF)(2));
+fprintf(f, '2 %d Tri3\n', size(elemsM)(2));
+fprintf(f, '2 %d Tri3\n', size(elemsC)(2));
+
+fprintf(f, '*INCIDENCES\n');
+
+for i = 1 : size(elemsF)(2)
+	fprintf(f, '%d %d %d %d\n', i, t(elemsF(1, i), 1), t(elemsF(1, i), 2), t(elemsF(1, i), 3))
+end
+
+for i = 1 : size(elemsM)(2)
+	fprintf(f, '%d %d %d %d\n', i, t(elemsM(1, i), 1), t(elemsM(1, i), 2), t(elemsM(1, i), 3))
+end
+
+for i = 1 : size(elemsC)(2)
+	fprintf(f, '%d %d %d %d\n', i, t(elemsC(1, i), 1), t(elemsC(1, i), 2), t(elemsC(1, i), 3))
+end
+
+fprintf(f, '*END\n');
+
+fclose(f);
